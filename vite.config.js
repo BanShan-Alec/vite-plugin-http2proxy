b6661ee6c7d7884a05eb48cb1e4794759f81a8d4
wengzehua <wengzehua@cvte.com>
2024-05-07 11:17:31 +0800

    feat: 更改包名

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  // https://cn.vitejs.dev/config/build-options.html#build-lib
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'http2Proxy',
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
